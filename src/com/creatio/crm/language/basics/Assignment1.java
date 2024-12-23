49a50eec68b398c3be370c903b591be22453e55d
Sai Kiran <saikiranpolimetla92@gmail.com>
2024-12-23 23:31:37 +0530

    Assignments

---
+++ src/com/creatio/crm/language/basics/Assignment1.java
@@ -0,0 +1,54 @@
+package com.creatio.crm.language.basics;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		double temperatureOfCityInCelcius = 25.5;
+		
+		     System.out.println("The temperature of the city in degrees Celcius is:" + temperatureOfCityInCelcius + "°C");
+		
+		long phoneNumber = 1234567890l;
+		
+		     System.out.println("Person's phone number is;" + phoneNumber);
+		
+		double customersBankBalance = 1000.50;
+		
+		     System.out.println("Amount of money in a customer's bank account:" + customersBankBalance );
+		
+		double coordinateLocation = 37.7749  -122.4194f;
+		
+		     System.out.println("Coordinates of a location (latitude, longitude):" + coordinateLocation);
+		
+		String personOcupation = "Software Engineer";
+		
+		     System.out.println("Person's Ocupation: " + personOcupation );
+		
+		String personEmail = "john.doe@example.com";
+		
+		     System.out.println("Person's email address:" + personEmail );
+		     
+		String favouriteColour = "Blue";
+		String currentYear = "2023";
+		String countryName = "United States";
+		String eyeColour = "Brown";
+		    
+		     System.out.println("Person Favourite Colour:" + favouriteColour);
+		     System.out.println("Curreny Year:" + currentYear);
+		     System.out.println("Country Name:" + countryName);
+		     System.out.println("eyeColour:" + eyeColour);
+		     
+		double twoCitiesDistance = 200.5;
+		double movieRating = 7.5;
+		double eventTime = 2.30;
+		     System.out.println("Distance between two cities:" + twoCitiesDistance );
+			 System.out.println("Movie Ratings:" + movieRating);
+			 System.out.println("Time of an event:" + eventTime +  "PM");
+			 
+				
+		
+		
+		
+	}
+
+}
